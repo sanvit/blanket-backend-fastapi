60816a73cd55d5e1d8f9f19b84cba9c0da92b744
Ian <ian@Ians-MacBook-Pro.local>
2022-07-26 20:18:14 +0900

    add: CORSMiddleware to fastapi

--- slack.py
+++ slack.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from enum import Enum
 import requests
@@ -8,6 +9,14 @@ from fastapi import Body
 
 
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post('/users')
